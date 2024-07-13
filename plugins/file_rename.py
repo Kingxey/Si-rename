22d1aa8fb12216d491e66a36ca4e6f87d062d3d2
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-07-13 13:31:38 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -1,6 +1,7 @@
 import random
 from helper.ffmpeg import fix_thumb, take_screen_shot
 from pyrogram import Client, filters
+from pyrogram.enums import MessageMediaType
 from pyrogram.types import ForceReply
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
@@ -16,23 +17,23 @@ from config import Config
 app = Client("test", api_id=Config.STRING_API_ID,
              api_hash=Config.STRING_API_HASH, session_string=Config.STRING_SESSION)
 
-# Directly prompt the user to enter the new file name
-@Client.on_message(filters.private & filters.incoming & filters.media)
-async def handle_file(client, message):
+# Directly prompt the user to enter the new file name for videos only
+@Client.on_message(filters.private & filters.incoming & filters.video)
+async def handle_video(client, message):
     await message.reply_text(
-        "__Pʟᴇᴀsᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇ Nᴀᴍᴇ...__",
-        reply_to_message_id=message.message_id,
+        "__Pʟᴇᴀsᴇ ᴇɴᴛᴇʀ ɴᴇᴡ ғɪʟᴇ ɴᴀᴍᴇ..__",
+        reply_to_message_id=message.id,
         reply_markup=ForceReply(True)
     )
 
-# Define the main message handler for private messages with replies
-@Client.on_message(filters.private & filters.reply)
+# Define the main message handler for private messages with replies for videos only
+@Client.on_message(filters.private & filters.reply & filters.video)
 async def refunc(client, message):
     reply_message = message.reply_to_message
     if isinstance(reply_message.reply_markup, ForceReply):
         new_name = message.text
         await message.delete()
-        msg = await client.get_messages(message.chat.id, reply_message.message_id)
+        msg = await client.get_messages(message.chat.id, reply_message.id)
         file = msg.reply_to_message
         media = getattr(file, file.media.value)
         if not "." in new_name:
@@ -60,12 +61,12 @@ async def start_conversion(client, message, file, new_name):
         # adding prefix and suffix
         new_filename = add_prefix_suffix(new_name, prefix, suffix)
     except Exception as e:
-        return await message.reply(f"⚠️ Sᴏᴍᴇᴛʜɪɴɢ Wᴇɴᴛ Wʀᴏɴɢ Cᴀɴ'ᴛ Aʙʟᴇ Tᴏ Sᴇᴛ Pʀᴇғɪx Oʀ Sᴜғғɪx ☹️ \n\n❄️ Cᴏɴᴛᴀᴄᴛ Mʏ Cʀᴇᴀᴛᴏʀ -> @Urr_Sanjii\nEʀʀᴏʀ: {e}")
+        return await message.reply(f"⚠️ Something went wrong can't able to set Prefix or Suffix ☹️ \n\n❄️ Contact My Creator -> @Urr_Sanjii\nError: {e}")
 
     file_path = f"downloads/{new_filename}"
-    ms = await message.reply("Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ...")
+    ms = await message.reply("Tʀyɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅɪɴɢ")
     try:
-        path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("\n⚠️ __**Pʟᴇᴀsᴇ Wᴀɪᴛ...**__\n\n🌨️ **Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....**", ms, time.time()))
+        path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("\n⚠️ __**Please wait...**__\n\n❄️ **Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....**", ms, time.time()))
     except Exception as e:
         return await ms.edit(e)
 
@@ -87,12 +88,12 @@ async def start_conversion(client, message, file, new_name):
 
             try:
                 if er:
-                    return await ms.edit(str(er) + "\n\n**Eʀʀᴏʀ**")
+                    return await ms.edit(str(er) + "\n\n**Error**")
             except BaseException:
                 pass
-        await ms.edit("**Mᴇᴛᴀᴅᴀᴛᴀ ᴀᴅᴅᴇᴅ ᴛᴏ ᴛʜᴇ ғɪʟᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ✅**\n\n⚠️ __**Tʀʏɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....**")
+        await ms.edit("**Metadata added to the file successfully ✅**\n\n⚠️ __**Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....**")
     else:
-        await ms.edit("⚠️  __**Pʟᴇᴀsᴇ Wᴀɪᴛ...**__\n\n\n**Tʀʏɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....**")
+        await ms.edit("⚠️  __**Please wait...**__\n\n\n**Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....**")
 
     duration = 0
     try:
@@ -162,7 +163,7 @@ async def start_conversion(client, message, file, new_name):
                 height=height,
                 duration=duration,
                 progress=progress_for_pyrogram,
-                progress_args=("⚠️ __**Pʟᴇᴀsᴇ Wᴀɪᴛ...**__\n\n🌨️ **Uᴩʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....**", ms, time.time()))
+                progress_args=("⚠️ __**Please wait...**__\n\n🌨️ **Uᴩʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....**", ms, time.time()))
         except Exception as e:
             os.remove(file_path)
             if ph_path:
